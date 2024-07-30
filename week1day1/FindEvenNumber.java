ae2675eb9cdf1fc23835c9cc74eeb2e9e8063e51
Tamilselvi-1312 <tamilselvi1312@gmail.com>
2024-07-30 17:22:10 +0530

    Add files via upload

---
+++ week1day1/FindEvenNumber.java
@@ -0,0 +1,15 @@
+package week1day1;
+
+public class FindEvenNumber {
+
+	public static void main(String[] args) {
+		int num=10;
+		for(int i=0;i<=num;i++) {
+			if(i%2==0) {
+				System.out.println("Even number is :" + i);
+			}
+		}
+
+	}
+
+}
